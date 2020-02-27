b6dfcffd54c63f2bb4a900717a780d87882afa42
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2020-02-27 16:56:47 -0700

    I Fixed it

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+function getUserProfile(req, res, next) {
+
+    try {
+      let profile = await profilesService.getProfile(req.userInfo);
+      res.send(profile);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  
+
+
+
+
 function yepItsGood(){
 
 }
