46bbc80dc7db814846f8e13e2ff066e5b798a77a
Jake Overall <joverall22@gmail.com>
2020-02-27 16:52:41 -0700

    cleanup

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-// function pleasePleaseWork(){
-//   //dskfsdakjflkdsjalkf jdljsfljdasl fjsa
-// }
-
-// function thisOneMightWOrk(){
-//   //dsafsadndsamfdslkanmkfldsj alfd
-// }
-// privatekey asdf8jdsioafu89je89jfasdkfds89j
-/**
- * 
- */
 function yepItsGood(){
 
 }
